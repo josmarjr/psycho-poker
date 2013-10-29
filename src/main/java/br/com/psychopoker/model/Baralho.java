07d37b1d72401bb7a604e20141b824b631d5c3c3
josmarjr <josmar.souzajr@gmail.com>
2013-10-28 22:24:46 -0200

    Criacao da classe Baralho, do metodo que contabiliza a repeticao das cartas e do metodo que verifica se ha uma dupla entre as 5 cartas

---
+++ src/main/java/br/com/psychopoker/model/Baralho.java
@@ -0,0 +1,65 @@
+package br.com.psychopoker.model;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import br.com.psychopoker.model.Carta.ValorCarta;
+
+
+public class Baralho {
+
+	private LinkedList<Carta> cartas;
+	private LinkedList<ValorCarta> valorCartas;
+	
+	public Baralho(List<Carta> cartas){
+		Collections.sort(cartas);
+		this.cartas = new LinkedList<Carta>();
+		this.cartas.addAll(cartas);
+		this.valorCartas = new LinkedList<ValorCarta>(Arrays.asList(ValorCarta.values()));
+	}
+	
+	/**
+	 * Método que verifica se há uma Dupla entre as 5 cartas.
+	 * 
+	 * @return
+	 */
+	public boolean hasDupla(){
+		if(this.cartas.size()>0){
+			for(Integer quantidade : getMapValores().values()){
+				if(quantidade.equals(2))
+					return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Método que retorna um Map com a quantidade de vezes que as cartas com
+	 * mesmo valor aparecem no baralho.
+	 * 
+	 * @return
+	 */
+	private Map<ValorCarta, Integer> getMapValores(){
+		
+		Map<ValorCarta, Integer> mapValores = new HashMap<ValorCarta, Integer>();
+
+		if (this.cartas.size() > 0) {
+			Iterator<Carta> itCartas = cartas.iterator();
+			while(itCartas.hasNext()){
+				Carta carta = itCartas.next();
+				Integer quantidade = mapValores.get(carta.getValor());
+				if (quantidade == null) {
+					mapValores.put(carta.getValor(), 1);
+				} else {
+					mapValores.put(carta.getValor(), quantidade + 1);
+				}
+			}
+		}
+		return mapValores;
+	}
+}
