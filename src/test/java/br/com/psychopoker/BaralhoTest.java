9ed682d4a31a917161ea0cd19a0285aff469a788
josmarjr <josmar.souzajr@gmail.com>
2013-10-28 22:18:11 -0200

    Testes arrumados para 5 cartas ao inves de 10

--- src/test/java/br/com/psychopoker/BaralhoTest.java
+++ src/test/java/br/com/psychopoker/BaralhoTest.java
@@ -11,42 +11,42 @@ public class BaralhoTest extends TestCase{
 	
 	public void testHasDupla(){
 		
-		Baralho baralho = new Baralho(getDeck("6C 9C 8C 2D 7C 2H TC 4C 9S AH"));
+		Baralho baralho = new Baralho(getDeck("7H 9C 8C 2D 7C"));
 		assertEquals(true, baralho.hasDupla());
 	}
 	
 	public void testHasDuasDuplas(){
-		Baralho baralho = new Baralho(getDeck("AH 2C 9S AD 3C QH KS JS JD KD"));
+		Baralho baralho = new Baralho(getDeck("AH 2C 9S AD 2H"));
 		assertEquals(true, baralho.hasDuasDuplas());
 	}
 	
 	public void testHasTrinca(){
-		Baralho baralho = new Baralho(getDeck("KS AH 2H 3C 4H KC 2C TC 2D AS"));
+		Baralho baralho = new Baralho(getDeck("KS KD KH 3C 4H"));
 		assertEquals(true, baralho.hasTrinca());
 	}
 
 	public void testHasStraight(){
-		Baralho baralho = new Baralho(getDeck("AC 2D 9C 3S KD 5S 4D KS AS 4C"));
+		Baralho baralho = new Baralho(getDeck("AC 2D 3S 5S 4D"));
 		assertEquals(true, baralho.hasStraight());
 	}
 	
 	public void testHasFlush(){
-		Baralho baralho = new Baralho(getDeck("2H AD 5H AC 7H AH 6H 9H 4H 3C"));
+		Baralho baralho = new Baralho(getDeck("2H KH 5H AH 7H"));
 		assertEquals(true, baralho.hasFlush());
 	}
 	
 	public void testHasFullHouse(){
-		Baralho baralho = new Baralho(getDeck("2H 2S 3H 3S 3C 2D 9C 3D 6C TH"));
+		Baralho baralho = new Baralho(getDeck("2H 2S 3H 3S 3C"));
 		assertEquals(true, baralho.hasFullHouse());
 	}
 	
 	public void testHasQuadra(){
-		Baralho baralho = new Baralho(getDeck("2H 2S 3H 3S 3C 2D 3D 6C 9C TH"));
+		Baralho baralho = new Baralho(getDeck("2H 3H 3S 3C 3D"));
 		assertEquals(true, baralho.hasQuadra());
 	}
 	
 	public void testHasStraightFlush(){
-		Baralho baralho = new Baralho(getDeck("TH JH QC QD QS QH KH AH 2S 6S"));
+		Baralho baralho = new Baralho(getDeck("TH JH QH KH AH"));
 		assertEquals(true, baralho.hasStraightFlush());
 	}
 	
