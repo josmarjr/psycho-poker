4a00463f0969d7696bf5ffb3ec5d39075bd08b67
josmarjr <josmar.souzajr@gmail.com>
2013-10-29 00:53:51 -0200

    Criacao do enum que representa as jogadas possiveis no poker

---
+++ src/main/java/br/com/psychopoker/model/Hands.java
@@ -0,0 +1,22 @@
+package br.com.psychopoker.model;
+
+public enum Hands {
+	HIGHEST_CARD("highest-card (maior carta)"), ONE_PAIR("one-pair (1 par)"), TWO_PAIRS(
+			"two-pairs (2 pares)"), THREE_OF_A_KIND("three-of-a-kind (trinca)"), STRAIGHT(
+			"straight (sequência numérica)"), FLUSH(
+			"flush (sequência de naipe)"), FULL_HOUSE(
+			"full-house (trinca + par)"), FOUR_OF_A_KIND(
+			"four-of-a-kind (quadra)"), STRAIGHT_FLUSH(
+			"straight-flush (sequência numérica e de naipe)");
+
+	private String value;
+
+	private Hands(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+}
